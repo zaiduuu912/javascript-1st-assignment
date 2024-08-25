f703af9fd5522dc4a857df578d2b007e1e2ffe34
zaid <zaid69232@gmail.com>
2024-08-25 12:33:36 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-//assignment no 1 (alert)
+// //assignment no 1 (alert)
 
-//question no 1
-alert("error! please enter a valid password")
-//question no 2
-alert("welcome to js land.. \n happy coding" )
-//question no 3
-alert("welcome to js land")
-//question no 4
-alert("happy coding \n prevent this page from creating additional dialogues")
-//question no 5
-alert("hello i can run js throughmy web browser's console")
+// //question no 1
+// alert("error! please enter a valid password")
+// //question no 2
+// alert("welcome to js land.. \n happy coding" )
+// //question no 3
+// alert("welcome to js land")
+// //question no 4
+// alert("happy coding \n prevent this page from creating additional dialogues")
+// //question no 5
+// alert("hello i can run js throughmy web browser's console")
 
 // //assignment no 2 (variables)
 // //question no 1
@@ -25,8 +25,47 @@ alert("hello i can run js throughmy web browser's console")
 // var email = ("my email address is example@example.com")
 // alert(email)
 
+// assignment no 3
+
+// question no 1
+var age =("i am 15 years old");
+alert(age);
+// question no 2
+var visited =("you have visited this site14 times")
+alert(visited)
+
+// assignment no 4
+
+// question no 1
+var rules = ("rules for naming js variables")
+var rules = ("rules for naming js variables")
+var rules = ("rules for naming js variables")
 
 
+document.right(rules)
+
+// question no 2
+
+// legal variables
+var userName;
+var age_25
+var isvalid
+var maxvalue
+var totalamount
+
+// illegal variables
+
+var my variables
+var 'var'
+var function
+var class - Like function, class
+
+// question no 3
+
+document.write("variable name can only contains.numbers.$ and _for example : $my 1st variable")
+document.write("variable must begin a letter,$ or_. for example :$name ,_name or name ")
+document.write("variables names are case sensitive")
+document.write("variables names should not be js keywords")
 
 
 
