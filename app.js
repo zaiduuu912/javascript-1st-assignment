affec80b375e0f233d495aeb2121d7c7439f5dd1
zaid <zaid69232@gmail.com>
2024-08-25 11:44:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//assignment no 1 (alert)
+
+//question no 1
+alert("error! please enter a valid password")
+//question no 2
+alert("welcome to js land.. \n happy coding" )
+//question no 3
+alert("welcome to js land")
+//question no 4
+alert("happy coding \n prevent this page from creating additional dialogues")
+//question no 5
+alert("hello i can run js throughmy web browser's console")
+
+// //assignment no 2 (variables)
+// //question no 1
+// var username = ("johndoe")
+// alert(username)
+// //question no 2
+// var myname = ("certified mobile application development")
+// alert(myname)
+// //question no 3
+// var res = ("pizza \n pizz \n piz \n pi \n p")
+// alert(res)
+// //question no 4
+// var email = ("my email address is example@example.com")
+// alert(email)
+
+
+
+
+
+
+
+
+
+
+
